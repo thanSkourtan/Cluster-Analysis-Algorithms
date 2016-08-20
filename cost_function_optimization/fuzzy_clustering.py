80dcd5c96b0df4833ea63348fee6f2938b1e3b7b
Than Skourtan <thanskourtan@gmail.com>
2016-08-20 13:18:55 +0300

    Inserted termination criterion in fuzzy clustering algorithm

--- cost_function_optimization/fuzzy_clustering.py
+++ cost_function_optimization/fuzzy_clustering.py
@@ -4,49 +4,58 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+euclidean_distance = lambda data, point: np.sqrt(np.sum(np.power(data - point, 2), axis = 1).reshape((len(data), 1)))
+
 def fuzzy(data, no_of_clusters, q = 1.25):
-    
     #debug
-    plt.scatter(data[:, 0], data[:, 1], c = 'b')
+    plt.scatter(data[:, 0], data[:, 1])
     partition_matrix = np.zeros((len(data), no_of_clusters))
-    #initialize centroids to 0
-    centroids = np.random.rand(no_of_clusters, 2)
-    #centroids = np.array([[6., 1379.],[5., 817.]])
-    #centroids = np.array([[9, 1]])
-    #euclidean between data array and point
-    euclidean_distance = lambda data, point: np.sqrt(np.sum(np.power(data - point, 2), axis = 1).reshape((len(data), 1)))
+    N = len(data)
     
-    #debug
-    colors = ['g', 'r', 'm']
-    #add the termination criterion
-    t = 0
-    while(t < 30):
-        for i in range(len(data)):
-            #precalculate euclidean distances for the instance. instance is the point, centroids are the data
-            eucl_dist = euclidean_distance(centroids, data[i,:])
+    #initialize centroids 
+    centroids_old = np.random.choice(np.arange(np.min(data), np.max(data)), size = (no_of_clusters, 2), replace = False)
+    centroids_new = np.zeros(centroids_old.shape) 
+    
+    # A do - while loop implementation in Python, as the algorithm needs to run at least once
+    condition = True
+    
+    while condition:
+        # Update the U matrix
+        for i in range(N):
+            # Precalculate euclidean distances for the instance.
+            eucl_dist = euclidean_distance(centroids_old, data[i,:])
             for j in range(no_of_clusters):
                 partition_matrix[i][j] = 1 / np.sum(np.power((1./eucl_dist) * eucl_dist[j, 0], (1/(q-1))))
         
-        #update the centroids
-        for i, centroid in enumerate(centroids):
-            centroids[i] = np.sum(np.power(partition_matrix[:,[i]], q) * data,axis = 0) / np.sum(np.power(partition_matrix[:,i], q))
-            
-        t += 1
-        #debug
-        print(centroids)
+        # Update the centroids
+        for i, centroid in enumerate(centroids_old):
+            centroids_new[i] = np.sum(np.power(partition_matrix[:,[i]], q) * data,axis = 0) / np.sum(np.power(partition_matrix[:,i], q))
         
-        plt.scatter(centroids[:, 0], centroids[:, 1], s = 35,c = 'r')
+        # Update the termination criterion where e = 0.00001
+        criterion_array = np.abs(centroids_new - centroids_old) < 0.00001
+        if np.any(criterion_array) :
+            condition = False
+           
+        centroids_old = np.copy(centroids_new)
+        
+        colors = ['g', 'r', 'm']
+        for i, c in enumerate(centroids_new):
+            plt.scatter(centroids_new[i, 0], centroids_new[i, 1], s = 80,c = colors[i], marker = 'x')
+
     plt.show()
+    
+    # Assign each vector to a cluster
     
     
     
     
     #ita calculation
     ita = []
-    for centroid in centroids:
+    for centroid in centroids_new:
         ita.append(np.sum(np.power(partition_matrix[:, [0]],q) * euclidean_distance(data, centroid)) / np.sum(np.power(partition_matrix[:, 0], 2)))
     
-    return partition_matrix, centroids, ita
+    
+    return partition_matrix, centroids_new, ita
     
 
 
---
+++ several_scripts/minimizing_cost_function.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+
+
+euclidean_distance = lambda data, point: np.sqrt(np.sum(np.power(data - point, 2), axis = 1).reshape((len(data), 1)))
+
+np.random.seed(seed=400)
+n = 60
+
+data = np.random.randint(n, size = (n, 2))
+
+# We are going to plot the cost against all possible values of theta
+theta1_x = np.arange(0,n)
+theta1_y = np.arange(0,n)
+
+# Build the weight matrix
+q = 3
+U = np.random.rand(n, 1)
+temp = np.ones((n, 1))
+U = np.power(np.hstack((U, temp-U)), q)
+
+cost = []
+centroid1_dimensions = np.zeros((np.power(len(theta1_x), 4), 2))
+centroid2_dimensions = np.zeros((np.power(len(theta1_x), 4), 2))
+i = 0 
+j = 0
+for x in theta1_x:
+    for y in theta1_y:
+        for k in theta1_x:
+            for m in theta1_y:
+                centroid1 = np.array([[x, y]])
+                centroid2 = np.array([[k, m]])
+                
+                centroid1_dimensions[i, 0] = x
+                centroid1_dimensions[i, 1] = y
+                centroid2_dimensions[j, 0] = k
+                centroid2_dimensions[j, 1] = m
+                i = i + 1
+                j = j + 1
+                
+                dist1 = euclidean_distance(data, centroid1)
+                dist2 = euclidean_distance(data, centroid2)
+                
+                distances = np.hstack((dist1, dist2)).T
+                cost.append(np.sum(np.dot(U, distances)))
+
+
+lala = np.array(centroid1_dimensions)
+np.array(centroid2_dimensions)
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.plot_trisurf(lala[:,0], lala[:,1], cost, cmap=cm.jet, linewidth=0.2)
+plt.show()
